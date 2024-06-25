eb622cb9ec93e14849537c4e971f952180647e41
ChinmayaGit <chinugarnaiklabs@gmail.com>
2024-06-25 15:01:34 +0530

    key test v1

--- src/services/Payment/index.js
+++ src/services/Payment/index.js
@@ -2,10 +2,11 @@ const { createPaymentOrder } = require('./function');
 const { checkOrderStatus } = require('./function');
 
 const createPaymentOrderHandler = async (req, res, next) => {
-    const { client_txn_id, amount, p_info, customer_name, customer_email, customer_mobile, redirect_url, udf1, udf2, udf3 } = req.body;
+    const {key, client_txn_id, amount, p_info, customer_name, customer_email, customer_mobile, redirect_url, udf1, udf2, udf3 } = req.body;
 
     try {
         const response = await createPaymentOrder({
+            key,
             client_txn_id,
             amount,
             p_info,
