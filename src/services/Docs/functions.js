b321ede2e09a028185fdfa3ebf4687b5f3b66dec
ChinmayaGit <chinugarnaiklabs@gmail.com>
2024-07-11 13:27:45 +0530

    new doc

--- src/services/Docs/functions.js
+++ src/services/Docs/functions.js
@@ -4,7 +4,7 @@ const dbServices = require("../../dao/queries/index");
 // get All PromoCode +
 const getAllDocument = async () => {
     try {
-      const dbResponse = await dbServices.PromoCode.getAllDocument();
+      const dbResponse = await dbServices.Docs.getAllDocument();
       return dbResponse;
     } catch (err) {
       throw new Error(err);
