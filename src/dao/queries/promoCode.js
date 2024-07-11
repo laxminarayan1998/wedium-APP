3b2e85f310c9f069c51c8ce11eb6bae4fee31d79
ChinmayaGit <chinugarnaiklabs@gmail.com>
2024-07-11 12:53:12 +0530

    create promo restart issue

--- src/dao/queries/promoCode.js
+++ src/dao/queries/promoCode.js
@@ -3,8 +3,11 @@ const promoCode = require("./model/promoCode");
 const mongoose = require("mongoose");
 
 const getAllPromoCode = async () => await promoCode.find();
-const createPromoCode = (promoCodeData) => promoCode.create(promoCodeData);
 
+const createPromoCode = async (promoCodeData) => {
+  const promoCode = new PromoCode(promoCodeData);
+  return await promoCode.save();
+};
 module.exports = {
     getAllPromoCode,
     createPromoCode,
